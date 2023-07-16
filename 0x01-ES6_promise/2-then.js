99dce807dfdab41b4857ed77801f8db2a675ca90
Zhakiya-00 <zakiyayusuf123@gmail.com>
2023-07-16 14:28:25 +0300

    then

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,10 +1,11 @@
-export default function handleResponseFromAPI(promise) {
-    const body = { status: 200, body: 'success' };
-  
-    return promise
-      .then(() => body)
-      .catch(() => Error())
-      .finally(() => {
-        console.log('Got a response from the API');
-      });
-  }
+function handleResponseFromAPI(promise) {
+  return promise
+    .then(() => ({
+      status: 200,
+      body: 'success',
+    }))
+    .catch(() => Error())
+    .finally(() => console.log('Got a response from the API'));
+}
+
+export default handleResponseFromAPI;
