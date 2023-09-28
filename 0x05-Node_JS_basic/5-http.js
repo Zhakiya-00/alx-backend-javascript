17b9800472c413d19f7a4723356f6ee7b0193749
sagebeme <eudicas5k@gmail.com>
2023-09-28 18:01:53 +0300

    the javascript code

--- 0x05-Node_JS_basic/5-http.js
+++ 0x05-Node_JS_basic/5-http.js
@@ -5,18 +5,18 @@ const countStudents = require('./3-read_file_async');
 
 const DATABASE = args[0];
 
-const host = '127.0.0.1';
+const hostname = '127.0.0.1';
 const port = 1245;
 
 const app = http.createServer(async (req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
+
   const { url } = req;
+
   if (url === '/') {
     res.write('Hello Holberton School!');
-    res.end();
-  }
-  if (url === '/students') {
+  } else if (url === '/students') {
     res.write('This is the list of our students\n');
     try {
       const students = await countStudents(DATABASE);
@@ -29,8 +29,8 @@ const app = http.createServer(async (req, res) => {
   res.end();
 });
 
-app.listen(port, host, () => {
-
+app.listen(port, hostname, () => {
+  //   console.log(`Server running at http://${hostname}:${port}/`);
 });
 
 module.exports = app;
